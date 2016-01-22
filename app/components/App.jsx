83121206c34829d94c1c4295f182e18ee4bdb721
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-01-22 14:40:02 +0100

    basic display task

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
-import Note from './Note.jsx'
+import uuid from 'node-uuid'
 
 export default class App extends React.Component {
 
     render() {
-        return <Note />
+        
+        const notes = [
+            {
+                id: uuid.v4(),
+                task: 'Learn Webpack'
+            },
+            {
+                id: uuid.v4(),
+                task: 'Learn React'
+            },
+            {
+                id: uuid.v4(),
+                task: 'Call mum'
+            }
+        ]
+
+        return (
+            <div>
+                <ul>
+                    { notes.map(note =>
+                        <li key={note.id}>{note.task}</li>
+                    )}
+                </ul>
+            </div>
+        )
     }
 }
