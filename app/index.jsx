a04b37a8763234c234c4e6e124d7b4f6c750b908
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-04-28 13:26:30 +0200

    persistance through localstorage

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,14 @@
-import './main.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './main.css'
+
+import alt from './libs/alt'
+import storage from './libs/storage'
+import persist from './libs/persist'
+
 import App from './components/App.jsx'
+
+
+persist(alt, storage, 'app')
 
 ReactDOM.render(<App />, document.getElementById('app'))
